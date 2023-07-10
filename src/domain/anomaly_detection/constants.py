8d0e8c9e451d1d1c6fda7e8b0d93d346039510c8
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-07-10 17:24:55 +0300

    ✨ Anomaly detection is integrated

--- src/domain/anomaly_detection/constants.py
+++ src/domain/anomaly_detection/constants.py
@@ -1,9 +1,17 @@
-from enum import StrEnum, auto
+import pickle
 
-__all__ = ("AnomalyDeviation",)
+from stumpy.aampi import aampi
+
+from src.config import settings
+
+__all__ = ("INITIAL_BASELINE_HIGH", "INITIAL_BASELINE_LOW")
 
 
-class AnomalyDeviation(StrEnum):
-    CRITICAL = auto()
-    WARNING = auto()
-    OK = auto()
+# Load initial baselines
+# --------------------------------------------------------------
+INITIAL_BASELINE_HIGH: aampi = pickle.load(
+    open(f"{settings.seed_dir}/baselines/high.mpstream", "rb")
+)
+INITIAL_BASELINE_LOW: aampi = pickle.load(
+    open(f"{settings.seed_dir}/baselines/low.mpstream", "rb")
+)
