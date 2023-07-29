f8a5c9ec1a13772bd720bb0abe7ecdd0d08197c1
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-07-29 17:21:26 +0300

    📝 Estimation domain root package documentation is added

--- src/domain/estimation/__init__.py
+++ src/domain/estimation/__init__.py
@@ -1,3 +1,11 @@
+"""
+This package includes represents the estimation after 
+all simulations for the specific sensor.
+
+It means that we simulate leaks by the specific sensor and then we
+can estimate the possibility for the specific sensor.
+"""
+
 from src.domain.estimation import services  # noqa: F401
 from src.domain.estimation.constants import *  # noqa: F401, F403
 from src.domain.estimation.models import *  # noqa: F401, F403
