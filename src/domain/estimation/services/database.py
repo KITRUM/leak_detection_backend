5cfcfd59cbd07435897828c20d4a2477f2de653e
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-07-29 11:53:39 +0300

    ✨ Estimation domain component prepared for consuming
    * The presentation component is implemented
    * Small changes to the domain are made
    * Data lake is updated
    * Startup / shutdown scripts are added

--- src/domain/estimation/services/database.py
+++ src/domain/estimation/services/database.py
@@ -1,3 +1,6 @@
+from sqlalchemy import delete
+
+from src.infrastructure.database import EstimationsSummariesTable
 from src.infrastructure.database.services.transaction import transaction
 
 from ..models import EstimationSummary, EstimationSummaryUncommited
@@ -7,3 +10,24 @@ from ..repository import EstimationsSummariesRepository
 @transaction
 async def save(schema: EstimationSummaryUncommited) -> EstimationSummary:
     return await EstimationsSummariesRepository().create(schema)
+
+
+@transaction
+async def delete_all():
+    """This function is used by the startup hook if debug mode is on."""
+
+    await EstimationsSummariesRepository().execute(
+        delete(EstimationsSummariesTable)
+    )
+
+
+@transaction
+async def get_historical_data(sensor_id: int) -> list[EstimationSummary]:
+    """Get the historical data."""
+
+    return [
+        instance
+        async for instance in EstimationsSummariesRepository().by_sensor(
+            sensor_id
+        )
+    ]
--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ from loguru import logger
 from src.application import anomaly_detection, estimation, simulation, tsd
 from src.config import settings
 from src.domain.anomaly_detection import services as anomaly_detection_services
+from src.domain.estimation import services as estimation_services
 from src.domain.simulation import services as simulation_services
 from src.domain.tsd import services as tsd_services
 from src.infrastructure import application
 from src.presentation.anomaly_detection import (
     router as anomaly_detection_router,
 )
+from src.presentation.estimation import router as estimation_summary_set_router
 from src.presentation.sensors import router as sensors_router
 from src.presentation.templates import router as templates_router
 from src.presentation.tsd import router as tsd_router
@@ -41,6 +43,7 @@ if settings.debug is True:
     shutdown_tasks.append(anomaly_detection_services.delete_all)
     shutdown_tasks.append(tsd_services.delete_all)
     shutdown_tasks.append(simulation_services.delete_all)
+    shutdown_tasks.append(estimation_services.delete_all)
 
 
 # Adjust the application
@@ -52,6 +55,7 @@ app: FastAPI = application.create(
         sensors_router,
         tsd_router,
         anomaly_detection_router,
+        estimation_summary_set_router,
     ),
     startup_tasks=[
         tsd.process_for_existed_sensors,
