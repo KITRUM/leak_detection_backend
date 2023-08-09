2e25fe7a67e4108d421e5e010261bdfb29a7967a
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-08-09 11:22:32 +0300

    🔥 SensorConfigurationPublic duplication is removed

--- src/presentation/sensors/contracts.py
+++ src/presentation/sensors/contracts.py
@@ -6,38 +6,6 @@ from src.infrastructure.models import PublicModel
 
 
 # ************************************************
-# ********** Sensors CRUD **********
-# ************************************************
-class SensorCreateRequestBody(PublicModel):
-    """This data model corresponds to the
-    http request body for sensor creation.
-    """
-
-    name: str = Field(description="The name of the sensor")
-    x: float = Field(description="The x position of the sensor")
-    y: float = Field(description="The y position of the sensor")
-    z: float = Field(description="The z position of the sensor")
-
-
-class SensorConfigurationPublic(PublicModel):
-    interactive_feedback_mode: bool = Field(
-        description=(
-            "This field reflects if the sensor works "
-            "in the interactive feedback mode."
-        ),
-    )
-
-
-class SensorPublic(SensorCreateRequestBody):
-    """The public sensor data model."""
-
-    id: int
-    configuration: SensorConfigurationPublic = Field(
-        description="The configuration of the sensor"
-    )
-
-
-# ************************************************
 # ********** SensorConfigurations CRUD ***********
 # ************************************************
 class SensorConfigurationUpdateRequestBody(PublicModel):
@@ -62,4 +30,27 @@ class SensorConfigurationPublic(PublicModel):
             "feature is turned on for that sensor. If the value is `false`,"
             "then, the anomaly detection processing will be ran in normal mode"
         )
+    )
+
+
+# ************************************************
+# ********** Sensors CRUD **********
+# ************************************************
+class SensorCreateRequestBody(PublicModel):
+    """This data model corresponds to the
+    http request body for sensor creation.
+    """
+
+    name: str = Field(description="The name of the sensor")
+    x: float = Field(description="The x position of the sensor")
+    y: float = Field(description="The y position of the sensor")
+    z: float = Field(description="The z position of the sensor")
+
+
+class SensorPublic(SensorCreateRequestBody):
+    """The public sensor data model."""
+
+    id: int
+    configuration: SensorConfigurationPublic = Field(
+        description="The configuration of the sensor"
     )
