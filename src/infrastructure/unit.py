179825c6b6e19775ace27d72f1b60cf719deca4a
Dmytro Parfeniuk <parfeniukink@gmail.com>
2023-08-10 15:58:28 +0300

    🔥 unit file is remove because of redundancy

--- src/infrastructure/unit.py
+++
@@ -1,6 +0,0 @@
-"""The units registry module."""
-from pint import UnitRegistry
-
-# NOTE: This constant should be used for all variables that have quantity
-#       and are used with different measurements in different cases.
-ureg = UnitRegistry()
