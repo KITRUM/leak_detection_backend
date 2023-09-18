280d5b78e75eccd891b5f83b0ab987cc38c02c19
Dmytro Parfeniuk <dmytro.parfeniuk@kitrum.com>
2023-09-18 17:25:06 +0300

    🔨 System events debug changes

--- src/presentation/events/system/websockets.py
+++ src/presentation/events/system/websockets.py
@@ -1,8 +1,12 @@
+import asyncio
+import random
+
 from fastapi import APIRouter, WebSocket
 from loguru import logger
 from websockets.exceptions import ConnectionClosed
 
 from src.application.data_lake import data_lake
+from src.domain.events.system import EventType
 from src.infrastructure.contracts import Response
 
 from .contracts import EventPublic
@@ -17,9 +21,14 @@ async def system_events(ws: WebSocket):
     await ws.accept()
     logger.success("Opening WS connection for system events fetching.")
 
-    # Run the infinite consuming of new system events
-    async for instance in data_lake.events_system.consume():
-        response = Response[EventPublic](result=EventPublic.from_orm(instance))
+    while True:
+        mock_event = EventPublic(
+            id=1, message="Mock", type=random.choice(list(EventType))
+        )
+
+        response = Response[EventPublic](
+            result=EventPublic.from_orm(mock_event)
+        )
 
         try:
             await ws.send_json(response.encoded_dict())
@@ -28,3 +37,5 @@ async def system_events(ws: WebSocket):
                 "Websocket events connection closed for system events fetching"
             )
             break
+
+        await asyncio.sleep(2)
